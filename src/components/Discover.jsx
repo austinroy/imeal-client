bd68d861680cb6f75fa9b9a89e6b91c5a10e1e51
Austin Roy <austin.roy@andela.com>
2018-10-14 21:20:55 +0300

    Add Discover Page to show public meals

---
+++ src/components/Discover.jsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import { Card, Table, Icon, Tab, Button, Rating } from 'semantic-ui-react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+import jwt from 'jsonwebtoken';
+import config from '../config';
+import { confirmAlert } from 'react-confirm-alert';
+import Notifications, {notify} from 'react-notify-toast';
+
+const API_URL = config.REACT_APP_API_URL;
+
+class Meals extends React.Component{
+  state = {
+    meals: []
+  }
+
+
+  updateMeals = (newMeals) => {
+    this.setState({ meals : newMeals });
+  }
+
+  rate = mealid => {
+    confirmAlert({
+      title: 'Confirm to submit',
+      message: 'Are you want to delete this?',
+      buttons: [
+        {
+          label: 'Yes',
+          onClick: () => this.rateMeal(mealid)
+        },
+        {
+          label: 'No',
+          onClick: () => alert("Changes not saved")
+        }
+      ]
+    })
+  }
+
+  MealsTable = meals => {
+    return (
+      <Table color="blue" style={{ width : '75%', margin : '0 auto' }}>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>
+              <Button color="blue" href="/addmeal"><Icon name="add" /></Button >
+            </Table.HeaderCell>
+            <Table.HeaderCell>Name</Table.HeaderCell>
+            <Table.HeaderCell>Category</Table.HeaderCell>
+            <Table.HeaderCell>Amount</Table.HeaderCell>
+            <Table.HeaderCell>Time Eaten</Table.HeaderCell>
+            <Table.HeaderCell>Rate</Table.HeaderCell>
+            <Table.HeaderCell></Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {meals.map(meal => {
+              const update_url = `/updatemeal/${meal._id}`;
+              return(
+                <Table.Row key={meal._id}>   
+                  <Table.Cell ></Table.Cell>
+                  <Table.Cell >{meal.name}</Table.Cell>
+                  <Table.Cell>{meal.category}</Table.Cell>
+                  <Table.Cell>{meal.amount}</Table.Cell>
+                  <Table.Cell>{meal.timeEaten}</Table.Cell>
+                  <Table.Cell>
+                    <Rating maxRating={5} onRate={this.handleRate} />
+                </Table.Cell>
+                </Table.Row>
+              )
+            })}
+        </Table.Body>
+      </Table>
+    )
+  }
+
+  fetchMeals = () => {
+    const token = localStorage.getItem('token');
+    const userData = jwt.decode(token);
+    const { id } = userData;
+    
+
+    axios.get(`${API_URL}/meals`,{
+      headers : {
+        'x-access-token' : token
+      }
+    })
+    .then( response => {
+      const fetchedMeals = response.data;
+      this.updateMeals(fetchedMeals);
+    })
+    .catch(function (error) {
+      notify.show('Error fetching meals', "error");
+      console.log(error);
+    });
+  }
+
+  componentDidMount(){
+    this.fetchMeals();
+  }
+
+  handleRate = (e, { rating }) => this.setState({ rating })
+
+  render(){
+    const { meals } = this.state;
+    return(
+      <div style={{ margin : '0 auto', alignItems: 'centre', width: '100%', margin: '0 auto', textAlign: 'center' }} >
+        <header><h3>Meals</h3></header>
+        <Notifications />
+        <br/>
+        {this.MealsTable(meals)}
+      </div>
+    )
+  }
+}
+
+export default Meals;
--- src/routes.jsx
+++ src/routes.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom'
 import App from './App';
 import Meals from './components/Meals';
+import Discover from './components/Discover';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import jwt from 'jsonwebtoken';
@@ -30,6 +31,7 @@ const Routes = () => (
             <Route exact path="/login" component={Login} />
             <Route exact path="/signup" component={SignUp} />
             <Route exact path="/meals" render={(props) => (!loggedIn() ? ( <Redirect to="/login"/> ): (<Meals {...props} />))} />
+            <Route exact path="/discover" render={(props) => (!loggedIn() ? ( <Redirect to="/login"/> ): (<Discover {...props} />))} />
             <Route exact path="/addmeal" render={(props) => (!loggedIn() ? ( <Redirect to="/login"/> ): (<AddMeal {...props} />))} />
             <Route path="/updatemeal/:meal_id" render={(props) => (!loggedIn() ? ( <Redirect to="/login"/> ): (<UpdateMeal {...props} />))} />
         </Switch>
